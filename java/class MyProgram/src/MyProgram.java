71a636e53100d670a7c9dbdffd67d5c377256c72
songkow <pastelmv@naver.com>
2022-07-22 00:39:02 +0900

    220721
    220721java

--- java/class MyProgram/src/MyProgram.java
+++ java/class MyProgram/src/MyProgram.java
@@ -1,5 +1,12 @@
-class MyProgram {
-}
-
-public class MyProgram2 {
+public class MyProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(7 % 3); // %는 나머지 1을 출력
+        System.out.println(3 % 7); // %는 나머지 3을 출력
+    }
 }
---
+++ java/sample/src/sample.java
@@ -0,0 +1,5 @@
+public class sample {
+        int i = 3;
+    boolean isOdd = i % 2 == 1;
+    System.out.println(isOdd);  // true 출력
+}
